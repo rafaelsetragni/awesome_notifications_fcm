828e30819bf11fc9b32e775d3e0d0bd4b2e57ed8
Rafael Setragni <rafaelsetra@gmail.com>
2024-10-16 08:47:22 -0300

    Prepare for Dart 3 plugin standards in Flutter
    This commit updates our Flutter project setup to adhere to the new Dart 3 plugin standards, anticipating future implementations. Changes include adjustments to analysis options, pubspec configurations, and other compatibility enhancements to ensure seamless integration once Dart 3 is fully adopted.

---
+++ test/awesome_notifications_fcm_method_channel_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:awesome_notifications_fcm/awesome_notifications_fcm_method_channel.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  MethodChannelAwesomeNotificationsFcm platform = MethodChannelAwesomeNotificationsFcm();
+  const MethodChannel channel = MethodChannel('awesome_notifications_fcm');
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+      channel,
+      (MethodCall methodCall) async {
+        return '42';
+      },
+    );
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await platform.getPlatformVersion(), '42');
+  });
+}
